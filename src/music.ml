d511d699added4236adb06b6b1eff2b714a575ff
Victoria Zhang <84301221+torizhangg@users.noreply.github.com>
2022-10-16 19:05:30 -0400

    implemented generate_seed  - created helper method generate_seed_helper

--- src/music.ml
+++ src/music.ml
@@ -4,3 +4,14 @@ open Yojson.Basic.Util
 type scale = { key : string; tonality : int list }
 type piano = { piano : char list }
 type notes = { notes : char list }
+type seed = int list
+
+let rec generate_seed_helper lst length range : seed =
+  if length = 0 then lst
+  else
+    let n = Random.int range in
+    let lst1 = lst @ [ n ] in
+    generate_seed_helper lst1 (length - 1) range
+
+let generate_seed (length : int) (range : int) : seed =
+  generate_seed_helper [] length range
