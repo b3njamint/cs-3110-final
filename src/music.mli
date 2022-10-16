a44d99da65428f9ec124f01c4d95e590ed2f0313
Sydney <sydney.tran@live.com>
2022-10-16 17:24:20 -0400

    Create interface music.mli

---
+++ src/music.mli
@@ -0,0 +1,21 @@
+(** Representation of melody generator. *)
+
+type scale = { key : string; tonality : int list }
+type piano = char list
+type notes = char list
+type seed = int list
+
+val piano_from_json : Yojson.Basic.t -> piano
+(** [piano_from_json j] is the notes that [j] contains. Requires: [j] is a valid. *)
+
+val scale_from_json : Yojson.Basic.t -> string -> string -> scale
+(** [scale_from_json j s k] is the scale with key [k] and tonality with name [s] in the json file [j] which contains all the different tonalities. Requires: [j] is a valid JSON tonalities representation. *)
+
+val generate_seed : int -> int -> seed
+(** [generate_indexes r l] is a list of random indexes of length [l] from range [0..r - 1]. *)
+
+val create_notes : piano -> scale -> notes
+(** [create_notes p s] are the possible notes to randomly choose from based on scale [s] and piano [p]. *)
+
+val create_melody : notes -> seed -> char list
+(** [create_melody notes indexes] is the melody created from the notes and the list of random indexes. *)
